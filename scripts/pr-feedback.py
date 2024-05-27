4e2c53865136ffb6c7b8be3e5229337fc1d53690
Fabien CHERER <fcherer@finaxys.com>
2024-05-27 10:52:13 +0200

    Premier éléments de script

---
+++ scripts/pr-feedback.py
@@ -0,0 +1,11 @@
+import argparse
+import os
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Process some files')
+    parser.add_argument('file_list', type=str, help='File list')
+    api_key = os.environ.get("OPENAI_API_KEY")
+    if api_key is None:
+        print("OPENAI_API_KEY environment variable must be set")
+    args = parser.parse_args()
+    print(args.file_list)
